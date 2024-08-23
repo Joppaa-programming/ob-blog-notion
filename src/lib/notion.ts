f77d392311da4968c29dd70c68353dee218b8854
AbrahamDN <abe.ndinga@gmail.com>
2024-08-23 08:59:00 +0100

    feat: add notion functions

---
+++ src/lib/notion.ts
@@ -0,0 +1,45 @@
+import { Client } from "@notionhq/client";
+import {
+  BlockObjectResponse,
+  PageObjectResponse,
+} from "@notionhq/client/build/src/api-endpoints";
+import React from "react";
+import "serve-only";
+
+export const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export const fetchPages = React.cache(() =>
+  notion.databases.query({
+    database_id: process.env.NOTION_DATABASE_ID!,
+    filter: {
+      property: "Status",
+      select: {
+        equals: "Done",
+      },
+    },
+  })
+);
+
+export const fetchBySlug = React.cache((slug: string) =>
+  notion.databases
+    .query({
+      database_id: process.env.NOTION_DATABASE_ID!,
+      filter: {
+        property: "Slug",
+        rich_text: {
+          equals: slug,
+        },
+      },
+    })
+    .then((res) => res.results[0] as PageObjectResponse | undefined)
+);
+
+export const fetchPageBlocks = React.cache((pageId: string) =>
+  notion.blocks.children
+    .list({
+      block_id: pageId,
+    })
+    .then((res) => res.results as BlockObjectResponse[])
+);
