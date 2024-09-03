18d6b4364afdf5d247fc0d313ed590dc28a41dc1
AbrahamDN <abe.ndinga@gmail.com>
2024-09-03 14:26:49 +0100

    refactor: add blogCard to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import BlogCard from "@/components/blogCard";
 import { fetchPages } from "@/lib/notion";
 import Link from "next/link";
 
@@ -5,20 +6,33 @@ export default async function Home() {
   const posts = await fetchPages();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between py-8 px-12 lg:px-24">
       <div className="">
         <h1 className="text-3xl md:text-6xl mb-4">OB Blog posts</h1>
 
-        {posts?.results?.map((post: any) => (
-          <Link
-            key={post.id}
-            href={`/blog/${post.properties.Slug?.formula.string}`}
-          >
-            <article>
-              <h3>{post.properties.Title.title[0].plain_text}</h3>
-            </article>
-          </Link>
-        ))}
+        <div className="grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+          {posts?.results?.map((post: any) => {
+            const imgFile = post.properties["Preview image"].files[0];
+            const img = imgFile.file?.url || imgFile.external?.url;
+            const title = post.properties.Title.title[0].plain_text;
+            const description =
+              post.properties.Description.rich_text[0].plain_text;
+
+            return (
+              <Link
+                key={post.id}
+                href={`/blog/${post.properties.Slug?.formula.string}`}
+              >
+                <BlogCard
+                  flex
+                  img={img}
+                  title={title}
+                  description={description}
+                />
+              </Link>
+            );
+          })}
+        </div>
       </div>
     </main>
   );
